7688349dfa45eeea041181bfac090c69db45e0a8
Joel Fragoso <joelfragoso85@icloud.com>
2021-04-15 11:11:10 -0300

    fix: add ThemeProvider to testUtils.tsx file

--- test/pages/index.test.tsx
+++ test/pages/index.test.tsx
@@ -1,17 +1,17 @@
 import React from 'react'
-import { render, fireEvent } from '../testUtils'
+import { render, fireEvent, screen } from '../testUtils'
 import Home from '../../pages/index'
 
 describe('Home page', () => {
-  it('matches snapshot', () => {
+  it('should be able to matches snapshot', () => {
     const { asFragment } = render(<Home />, {})
     expect(asFragment()).toMatchSnapshot()
   })
 
-  it('clicking button triggers alert', () => {
-    const { getByText } = render(<Home />, {})
-    window.alert = jest.fn()
-    fireEvent.click(getByText('Test Button'))
-    expect(window.alert).toHaveBeenCalledWith('With typescript and Jest')
+  it('should be able to click in button', () => {
+    render(<Home />, {})
+    const button = screen.getByRole('button', { name: 'Change Theme Mode' })
+    fireEvent.click(button)
+    expect(button).toBeDefined()
   })
 })
